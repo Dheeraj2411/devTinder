4f83d23557382a4f604da8f70da6c705cfe3ebff
Dheeraj2411 <dheerajjha834@gmail.com>
2025-09-25 21:39:05 +0530

    http req

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+
+app.get("/user", (req, res) => {
+  res.send({ FirstName: "Dheeraj", lastName: "Jha" });
+});
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User data successfully deleted");
+});
 app.use("/test", (req, res) => {
   res.send("Hello world from server");
 });
